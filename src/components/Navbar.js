5c912f598ae96a7b91338148f22e63809248e609
konchu974 <156214774+konchu974@users.noreply.github.com>
2025-05-02 12:24:34 +0200

    logique d'affiche des projets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,17 +10,8 @@ function Navbar() {
 </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="sass.html">A propos</a></li>
-          <li><a href="badges.html">Projets</a></li>
-          <li>
-            <div className="dropdown">
-              <div className="dropdown-toggle">Centres d'Intérêt</div>
-              <div className="dropdown-options">
-                <a href="/gallery">Photo</a>
-                <a href="#">Modélisation 3D</a>
-                <a href="#">Je sais pas</a>
-              </div>
-            </div>
-          </li>
+          <li><a href="/project">Projets</a></li>
+          <li><a href="/gallery">Centres d'Intérêt</a></li>
           <li><a href="badges.html">Contact</a></li>
         </ul>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,85 +1,66 @@
-import React from 'react';
-import '../Css/Projects.css'; // Assure-toi d’avoir les styles correspondants
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../Css/Projects.css';
+
+const API_URL = "http://localhost:5000/api/projects";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchProjects = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(API_URL);
+      setProjects(response.data.projects); // Suppose que l'API renvoie { projects: [...] }
+      setError("");
+    } catch (err) {
+      console.error(err);
+      setError("Erreur lors du chargement des projets.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  if (loading) return <p>Chargement en cours...</p>;
+  if (error)
+    return (
+      <div>
+        <p>{error}</p>
+        <button onClick={fetchProjects}>Réessayer</button>
+      </div>
+    );
+
   return (
     <section id="projects">
       <div className="projcard-container">
-
-        <div className="projcard projcard-blue">
-          <div className="projcard-innerbox">
-            <img className="projcard-img" src="https://picsum.photos/800/600?image=1041" alt="Project 1" />
-            <div className="projcard-textbox">
-              <div className="projcard-title">Card Title</div>
-              <div className="projcard-subtitle">This explains the card in more detail</div>
-              <div className="projcard-bar"></div>
-              <div className="projcard-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-              </div>
-              <div className="projcard-tagbox">
-                <span className="projcard-tag">HTML</span>
-                <span className="projcard-tag">CSS</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="projcard projcard-red">
-          <div className="projcard-innerbox">
-            <img className="projcard-img" src="https://picsum.photos/800/600?image=1080" alt="Project 2" />
-            <div className="projcard-textbox">
-              <div className="projcard-title">That's Another Card</div>
-              <div className="projcard-subtitle">No explanation needed here</div>
-              <div className="projcard-bar"></div>
-              <div className="projcard-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-              </div>
-              <div className="projcard-tagbox">
-                <span className="projcard-tag">PHP</span>
-                <span className="projcard-tag">SQL</span>
-                <span className="projcard-tag">Database</span>
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className={`projcard ${project.colorClass}`}
+            style={project.customColor ? { '--projcard-color': project.customColor } : {}}
+          >
+            <div className="projcard-innerbox">
+              <img className="projcard-img" src={project.image} alt={project.title} />
+              <div className="projcard-textbox">
+                <div className="projcard-title">{project.title}</div>
+                <div className="projcard-subtitle">{project.subtitle}</div>
+                <div className="projcard-bar"></div>
+                <div className="projcard-description">{project.description}</div>
+                <div className="projcard-tagbox">
+                  {project.technologies.map((tech, i) => (
+                    <span key={i} className="projcard-tag">{tech}</span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="projcard projcard-green">
-          <div className="projcard-innerbox">
-            <img className="projcard-img" src="https://picsum.photos/800/600?image=1039" alt="Project 3" />
-            <div className="projcard-textbox">
-              <div className="projcard-title">And a Third Card</div>
-              <div className="projcard-subtitle">You know what this is</div>
-              <div className="projcard-bar"></div>
-              <div className="projcard-description">
-                Duis aute irure dolor in reprehenderit in voluptate...
-              </div>
-              <div className="projcard-tagbox">
-                <span className="projcard-tag">Excel</span>
-                <span className="projcard-tag">VBA</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="projcard projcard-customcolor" style={{ '--projcard-color': '#F5AF41' }}>
-          <div className="projcard-innerbox">
-            <img className="projcard-img" src="https://picsum.photos/800/600?image=943" alt="Project 4" />
-            <div className="projcard-textbox">
-              <div className="projcard-title">Last Card</div>
-              <div className="projcard-subtitle">That's the last one. Have a nice day!</div>
-              <div className="projcard-bar"></div>
-              <div className="projcard-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-              </div>
-              <div className="projcard-tagbox">
-                <span className="projcard-tag">iOS</span>
-                <span className="projcard-tag">Android</span>
-                <span className="projcard-tag">Cordova</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        ))}
       </div>
     </section>
   );
